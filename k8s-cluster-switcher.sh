b59ba6284be723ea38e8b2516632d73a9a46d82d
Anton Bokovets <anton@radlogics.com>
2021-02-03 21:30:19 +0300

    Some changes

--- k8s-cluster-switcher.sh
+++ k8s-cluster-switcher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $1 = 'cc' ];
+if [[ $1 = 'cc' ]];
 then 
   echo Current context: $(kubectl config current-context)
 elif [ -z $1 ]; # -z True if the length of string is zero.
@@ -18,9 +18,9 @@ then
   echo "==============================================================="
   read -p "Select the context you want to work with. Your choice must be between 1 and ${#array[*]}: " varname
   
-  # new_varname = int($varname) + 1
-  # echo $new_varname
-  echo kubectl was configured to interact with context: ${array[$varname]}
+  new_varname=$(($varname-1))
+  echo $new_varname
+  echo kubectl was configured to interact with context: $varname '-' ${array[$new_varname]}
 else 
   echo "Run 'ksw --help' for usage"
 fi
