096848f2ef6448fe99462045706d2156f44c0e8b
Anton Bokovets <anton@radlogics.com>
2021-02-12 21:28:43 +0300

    Some changes

--- k8s-cluster-switcher.sh
+++ k8s-cluster-switcher.sh
@@ -19,8 +19,13 @@ then
   read -p "Select the context you want to work with. Your choice must be between 1 and ${#array[*]}: " varname
   
   new_varname=$(($varname-1))
-  echo $new_varname
+  kubectl config use-context ${array[$new_varname]}
   echo kubectl was configured to interact with context: $varname '-' ${array[$new_varname]}
+elif [ $1 = 'ds' ]; # -z True if the length of string is zero.
+then
+  kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-user | awk '{print $1}')
+  echo "kubectl configured dashboard for context: $(kubectl config current-context)"
+  kubectl proxy
 else 
   echo "Run 'ksw --help' for usage"
 fi
