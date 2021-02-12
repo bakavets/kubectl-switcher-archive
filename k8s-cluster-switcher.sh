67bcc31420c0ebb71dd788adf6b6ab76d3f88b1f
Anton Bokovets <anton@radlogics.com>
2021-02-13 00:04:19 +0300

    Dev

--- k8s-cluster-switcher.sh
+++ k8s-cluster-switcher.sh
@@ -18,6 +18,15 @@ then
   echo "==============================================================="
   read -p "Select the context you want to work with. Your choice must be between 1 and ${#array[*]}: " varname
   
+  while [[ ! $varname =~ ^-?[0-9]+$ ]]; do
+      # if [[ $varname > 0 || $varname -gt ${#array[*]} ]];
+      # then
+      #   echo 'Your choice must be from 1 to' ${#array[*]}
+      # fi
+      echo "Your choice must be a number!"
+      read -p "Select the context you want to work with. Your choice must be between 1 and ${#array[*]}: " varname
+  done
+
   new_varname=$(($varname-1))
   kubectl config use-context ${array[$new_varname]}
   echo kubectl was configured to interact with context: $varname '-' ${array[$new_varname]}
@@ -26,6 +35,12 @@ then
   kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-user | awk '{print $1}')
   echo "kubectl configured dashboard for context: $(kubectl config current-context)"
   kubectl proxy
+elif [[ $1 = '--help' ]]; # -z True if the length of string is zero.
+then
+  echo "
+ksw cc - Show current context;
+ksw ds - Retrieve token for dashboard user and run kubectl proxy for the current context;
+        "
 else 
   echo "Run 'ksw --help' for usage"
 fi
